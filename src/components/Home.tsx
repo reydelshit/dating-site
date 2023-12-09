d0918ce79b642427073728461057c1e5c8cdb2f0
Reydel Ocon <reydel321@gmail.com>
2023-12-09 17:10:37 +0800

    yes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 type ProfileMatched = {
   age: string;
@@ -16,14 +17,62 @@ type ProfileMatched = {
   profile_id: number;
   year: string;
 };
+
+type MessageType = {
+  created_at: string;
+  message_context: string;
+  receiver_id: number;
+  sender_id: number;
+  profile: string;
+  sender_username: string;
+  profile_picture: string;
+};
+
 export default function Home() {
   const [showFindMatch, setShowFindMatch] = useState(false);
   const [interest, setInterest] = useState<ProfileMatched[]>([]);
+  const [message, setMessage] = useState<MessageType[]>([]);
 
   const credential_id = localStorage.getItem('dating_site_id');
 
   const [currentProfileIndex, setCurrentProfileIndex] = useState(0);
   const [swipeDirection, setSwipeDirection] = useState(null) as any;
+
+  const [profileMatched, setProfileMatched] = useState<ProfileMatched[]>([]);
+  const [recepientIDNumber, setRecepientIDNumber] = useState(0);
+  const [recepientMessage, setRecepientMessage] = useState<MessageType[]>([]);
+  const [messageSent, setMessageSent] = useState('');
+  const [showMessageRecepient, setShowMessageRecepient] = useState(false);
+
+  const handleShowMessage = (id: number) => {
+    setRecepientIDNumber(id);
+    axios
+      .get(`${import.meta.env.VITE_DATING_SITE}/message-fetch.php`, {
+        params: {
+          sender_id: credential_id,
+          receiver_id: id,
+        },
+      })
+      .then((res) => {
+        console.log(res.data, 'get message recepient');
+        setRecepientMessage(res.data);
+        setShowMessageRecepient(true);
+      });
+  };
+
+  const fetchMessageRecepient = () => {
+    axios
+      .get(`${import.meta.env.VITE_DATING_SITE}/message-fetch.php`, {
+        params: {
+          sender_id: credential_id,
+          receiver_id: recepientIDNumber,
+        },
+      })
+      .then((res) => {
+        console.log(res.data, 'get message recepient');
+        setRecepientMessage(res.data);
+      });
+  };
 
   const fetchInterestFromProfile = () => {
     axios
@@ -52,8 +101,41 @@ export default function Home() {
       });
   };
 
+  const getRecepientMessage = async () => {
+    axios
+      .get(`${import.meta.env.VITE_DATING_SITE}/message.php`, {
+        params: { sender_id: credential_id },
+      })
+      .then((res) => {
+        console.log(res.data, 'message');
+        // setRecepientMessage(res.data);
+
+        if (Array.isArray(res.data) && res.data.length > 0) {
+          setMessage(res.data);
+        } else {
+          setMessage([]);
+        }
+      });
+  };
+
+  // const getMessageRecepient = () => {
+  //   axios
+  //     .get(`${import.meta.env.VITE_DATING_SITE}/message-fetch.php`, {
+  //       params: {
+  //         sender_id: credential_id,
+  //         receiver_id: 3,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data, 'get message recepient');
+  //       setRecepientMessage(res.data);
+  //     });
+  // };
+
   useEffect(() => {
     fetchInterestFromProfile();
+    getRecepientMessage();
+    // getMessageRecepient();
   }, []);
 
   const handleSwipeLeft = () => {
@@ -70,6 +152,38 @@ export default function Home() {
     }
   };
 
+  const handleChatNowReceipient = (id: number) => {
+    console.log(id);
+    axios
+      .get(`${import.meta.env.VITE_DATING_SITE}/profile.php`, {
+        params: {
+          credential_id: id,
+        },
+      })
+      .then((res) => {
+        console.log(res.data, 'chat');
+        setProfileMatched(res.data);
+        setRecepientIDNumber(id);
+        setShowFindMatch(false);
+      });
+  };
+
+  const handleMessageSent = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    axios
+      .post(`${import.meta.env.VITE_DATING_SITE}/message.php`, {
+        sender_id: Number(credential_id),
+        receiver_id: recepientIDNumber,
+        message_context: messageSent,
+      })
+      .then((res) => {
+        console.log(res.data, 'message sent');
+        fetchMessageRecepient();
+        setMessageSent('');
+      });
+  };
+
   return (
     <div className="h-screen w-full relative">
       <div className="h-fit flex relative">
@@ -83,96 +197,171 @@ export default function Home() {
             </Button>
           </div>
 
-          <div className="flex items-center gap-4 bg-white border-2 p-2 rounded-md my-2">
-            <img
-              src={Default}
-              alt="default"
-              className="w-[6rem] h-[6rem] object-cover rounded-full"
-            />
+          {profileMatched.length > 0 &&
+            profileMatched.map((profile, index) => (
+              <div
+                key={index}
+                className="flex items-center gap-4 bg-white border-2 p-2 rounded-md my-2 h-[8rem] "
+              >
+                <img
+                  src={profile.profile}
+                  alt="default"
+                  className="w-[6rem] h-[6rem] object-cover rounded-full"
+                />
 
-            <div>
-              <h1 className="text-2xl font-bold cursor-pointer">Reydel Ocon</h1>
-              <p>broo i miss you ..</p>
-            </div>
-          </div>
+                <div>
+                  <h1 className="text-2xl font-bold cursor-pointer">
+                    {profile.fullname}
+                  </h1>
+                  <p>broo i miss you ..</p>
+                </div>
+              </div>
+            ))}
 
-          <div className="flex items-center gap-4 bg-white border-2 p-2 rounded-md my-2">
-            <img
-              src={Default}
-              alt="default"
-              className="w-[6rem] h-[6rem] object-cover rounded-full"
-            />
+          <div>
+            {message.length > 0 ? (
+              message
+                .filter(
+                  (mes) => Number(mes.sender_id) !== Number(credential_id),
+                )
+                .map((mes, index) => {
+                  return (
+                    <div
+                      className="border-2 p-2 mt-[1rem] rounded-sm bg-gray-200"
+                      key={index}
+                    >
+                      <div className="flex items-center gap-4">
+                        <img
+                          className="w-[5rem] h-[5rem] rounded-full object-cover"
+                          src={mes.profile}
+                        />
+                        <div className="flex flex-col w-full">
+                          <span className="flex justify-between w-full">
+                            <h1
+                              onClick={() => handleShowMessage(mes.sender_id)}
+                              className="font-bold cursor-pointer"
+                            >
+                              {mes.sender_username}
+                            </h1>
+                            <p className="text-xs">
+                              {moment(mes.created_at).format('LLL')}
+                            </p>
+                          </span>
 
-            <div>
-              <h1 className="text-2xl font-bold cursor-pointer">Reydel Ocon</h1>
-              <p>broo i miss you ..</p>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-4 bg-white border-2 p-2 rounded-md my-2">
-            <img
-              src={Default}
-              alt="default"
-              className="w-[6rem] h-[6rem] object-cover rounded-full"
-            />
-
-            <div>
-              <h1 className="text-2xl font-bold cursor-pointer">Reydel Ocon</h1>
-              <p>broo i miss you ..</p>
-            </div>
+                          <p>{mes.message_context}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+            ) : (
+              <p>No Message</p>
+            )}
           </div>
         </div>
         <div className="w-full border-2 h-full px-4">
           <div className="flex flex-col">
-            <div className="flex items-center gap-4 bg-white border-2 p-2 rounded-md my-2">
-              <img
-                src={Default}
-                alt="default"
-                className="w-[6rem] h-[6rem] object-cover rounded-full"
-              />
+            {profileMatched.length > 0 &&
+              profileMatched.map((profile, index) => (
+                <div
+                  key={index}
+                  className="flex items-center gap-4 bg-white border-2 p-2 rounded-md my-2"
+                >
+                  <img
+                    src={profile.profile}
+                    alt="default"
+                    className="w-[6rem] h-[6rem] object-cover rounded-full"
+                  />
 
-              <div>
-                <h1 className="text-2xl font-bold cursor-pointer">
-                  Reydel Ocon, 17 Male / BSIT
+                  <div>
+                    <h1 className="text-2xl font-bold cursor-pointer">
+                      {profile.fullname +
+                        ', ' +
+                        profile.age +
+                        ' ' +
+                        profile.gender +
+                        ' ' +
+                        profile.course}
+                    </h1>
+                    <p>{profile.looking_for}</p>
+                  </div>
+                </div>
+              ))}
+
+            {showMessageRecepient && (
+              <>
+                <div className="flex items-center justify-between border-b-2 p-2">
+                  {recepientMessage
+                    .filter(
+                      (mes) => Number(mes.sender_id) !== Number(credential_id),
+                    )
+                    .map((res, index) => {
+                      return (
+                        <div key={index} className="flex items-center gap-2">
+                          <img
+                            className="w-[6rem] h-[6rem] rounded-full object-cover"
+                            src={res.profile_picture}
+                            alt=""
+                          />
+                          <h1 className="font-bold">{res.sender_username}</h1>
+                        </div>
+                      );
+                    })
+                    .slice(0, 1)}
+                </div>
+                <div className="overflow-x-auto h-fit">
+                  {recepientMessage.map((res, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className={`w-full flex ${
+                          Number(res.sender_id) === Number(credential_id)
+                            ? 'justify-end'
+                            : 'justify-start'
+                        } my-2`}
+                      >
+                        <div
+                          className={`w-fit  ${
+                            Number(res.sender_id) === Number(credential_id)
+                              ? 'bg-red-500 text-white'
+                              : 'bg-white'
+                          } border-2 p-2 rounded-md`}
+                        >
+                          <p>{res.message_context}</p>
+                          <p className="text-xs">
+                            {moment(res.created_at).format('LLL')}
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+
+                <div className="my-[2rem] flex gap-4 w-full">
+                  <form
+                    className="w-full flex gap-4 items-center"
+                    onSubmit={handleMessageSent}
+                  >
+                    <Input
+                      className="h-[4rem]"
+                      value={messageSent}
+                      onChange={(e) => setMessageSent(e.target.value)}
+                      placeholder="Type your message here"
+                    />
+
+                    <Button type="submit">Send</Button>
+                  </form>
+                </div>
+              </>
+            )}
+
+            {!showMessageRecepient && profileMatched.length === 0 && (
+              <div className="w-full flex justify-center items-center h-[80vh]">
+                <h1 className="font-bold text-3xl">
+                  FIND MATCH AND START CHATTING 🥰😍🥰
                 </h1>
-                <p>Long Term</p>
               </div>
-            </div>
-
-            <div className="w-full">
-              <div className="w-full flex justify-start my-2">
-                <div className="w-fit bg-white border-2 p-2 rounded-md">
-                  <p>HWAHHAWHWAHAWHAWHHWAHWAHWAHAWHHWAHAW</p>
-                  <span>January, 31 2023 1:00 pm</span>
-                </div>
-              </div>
-
-              <div className="w-full flex justify-end my-2">
-                <div className="w-fit bg-red-500 text-white border-2 p-2 rounded-md">
-                  <p>HWAHHAWHWAHAWHAWHHWAHWAHWAHAWHHWAHAW</p>
-                  <span>January, 31 2023 1:00 pm</span>
-                </div>
-              </div>
-
-              <div className="w-full flex justify-end my-2">
-                <div className="w-fit bg-red-500 text-white border-2 p-2 rounded-md">
-                  <p>HWAHHAWHWAHAWHAWHHWAHWAHWAHAWHHWAHAW</p>
-                  <span>January, 31 2023 1:00 pm</span>
-                </div>
-              </div>
-
-              <div className="w-full flex justify-start my-2">
-                <div className="w-fit bg-white border-2 p-2 rounded-md">
-                  <p>HWAHHAWHWAHAWHAWHHWAHWAHWAHAWHHWAHAW</p>
-                  <span>January, 31 2023 1:00 pm</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="my-[2rem] flex gap-4">
-              <Input placeholder="Type your message here" />
-              <Button>Send</Button>
-            </div>
+            )}
           </div>
         </div>
       </div>
@@ -214,7 +403,7 @@ export default function Home() {
 
                         <div className="absolute top-0 bg-white w-full p-2 h-[4rem] bg-opacity-70 flex justify-between items-center z-90">
                           <h1 className="text-2xl font-bold cursor-pointer text-red-500">
-                            Something Casual
+                            {profileMatched.looking_for}
                           </h1>
 
                           <Button
@@ -244,7 +433,14 @@ export default function Home() {
                               profileMatched.gender}
                           </h1>
                           <div className="mt-[1rem] flex justify-center">
-                            <Button className="h-[3rem] w-[8rem] z-50">
+                            <Button
+                              onClick={() =>
+                                handleChatNowReceipient(
+                                  profileMatched.credential_id,
+                                )
+                              }
+                              className="h-[3rem] w-[8rem] z-50"
+                            >
                               Chat now
                             </Button>
                           </div>
@@ -255,7 +451,18 @@ export default function Home() {
                 })
             ) : (
               <div className="flex justify-center items-center w-[40rem] bg-white border-2 h-[45rem] object-cover mt-[5rem] relative rounded-xl overflow-hidden">
-                <div className="absolute inset-0 flex justify-center items-center z-50">
+                <div className="absolute top-0 bg-white w-full p-2 h-[4rem] bg-opacity-70 flex justify-between items-center z-90">
+                  <h1 className="text-2xl font-bold cursor-pointer text-red-500"></h1>
+
+                  <Button
+                    onClick={() => setShowFindMatch(false)}
+                    className="cursor-pointer z-50"
+                  >
+                    Close
+                  </Button>
+                </div>
+
+                <div className="absolute inset-0 flex justify-center items-center">
                   <div className="absolute left-0 flex items-center p-2">
                     <Button onClick={handleSwipeLeft}>Swipe Left</Button>
                   </div>
@@ -263,7 +470,11 @@ export default function Home() {
                     <Button onClick={handleSwipeRight}>Swipe Right</Button>
                   </div>
                 </div>
-                <h1 className="text-center"> No more profiles available</h1>
+                <div>
+                  <h1 className="break-words w-[100%]">
+                    No match or no more profiles available
+                  </h1>
+                </div>
               </div>
             )
           ) : (
